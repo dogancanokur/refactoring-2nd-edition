bb596028c234491c43230baaceb897d7e71843ef
dogancan.okur <dogancan.okur@gmail.com>
2023-05-30 23:47:37 +0300

    chapter01 phase01-05 removing the "format" variable

--- chapter01/refactor-statement.js
+++ chapter01/refactor-statement.js
@@ -26,25 +26,27 @@ function volumeCreditsFor(perf) {
     return result;
 }
 
+function getUsdFormat(number) {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency", currency: "USD", minimumFractionDigits: 2,
+    }).format(number);
+}
+
 function statement(invoice) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency", currency: "USD", minimumFractionDigits: 2,
-    }).format;
 
     for (let perf of invoice.performances) {
 
         volumeCredits = volumeCreditsFor(perf);
 
         // print line for this order
-        result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+        result += `  ${playFor(perf).name}: ${getUsdFormat(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
 
-    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+    result += `Amount owed is ${getUsdFormat(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
 
     return result;
