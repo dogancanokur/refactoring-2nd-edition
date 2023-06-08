75600d4621b1391a5aba8e65f99e1467239a8bbf
dogancan.okur <dogancan.okur@gmail.com>
2023-06-09 02:12:45 +0300

    chapter01 phase02-03 htmlRenderer

--- chapter01/refactor-statement.js
+++ chapter01/refactor-statement.js
@@ -36,3 +36,21 @@ function statement(invoice) {
 }
 
 console.log(statement(invoice[0]));
+
+function htmlStatement(invoice) {
+    return renderHtml(createStatementData(invoice));
+}
+
+function renderHtml(data) {
+    let result = `<h1>Statement for ${data.customer}</h1><table><tr><th>play</th><th>seats</th><th>cost</th></tr>`;
+    for (let perf of data.performances) {
+        result += `  <tr><td>${playFor(perf).name}</td><td>${perf.audience}</td>`;
+        result += `<td>${getUsdFormat(createPerformanceCalculator(perf, playFor(perf)).amount / 100)}</td></tr>\n`;
+    }
+    result += `</table>
+<p>Amount owed is <em>${getUsdFormat(data.totalAmount / 100)}</em></p>
+<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+    return result;
+}
+
+console.log(htmlStatement(invoice[0]));
